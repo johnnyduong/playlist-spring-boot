d08832b1dd53e3487f91bd72a54919a82518d4c3
Johnny Duong <johnnyduong@live.com>
2022-01-13 10:24:43 -0800

    Spring Boot Initializr

---
+++ src/main/java/com/projects/music/SpringBootMusicApplication.java
@@ -0,0 +1,13 @@
+package com.projects.music;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMusicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMusicApplication.class, args);
+	}
+
+}
