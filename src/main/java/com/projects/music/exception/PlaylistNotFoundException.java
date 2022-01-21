bfa995d009447fd62306cba48bb05adeb3cb72ba
Johnny Duong <johnnyduong@live.com>
2022-01-21 10:23:16 -0800

    Added error exception handling

---
+++ src/main/java/com/projects/music/exception/PlaylistNotFoundException.java
@@ -0,0 +1,11 @@
+package com.projects.music.exception;
+
+import java.math.BigInteger;
+
+@SuppressWarnings("serial")
+public class PlaylistNotFoundException extends RuntimeException {
+
+	public PlaylistNotFoundException(final BigInteger id) {
+		super(String.format("playlist with id '%s' not found", id));
+	}
+}
---
+++ src/main/java/com/projects/music/exception/SongNotFoundException.java
@@ -0,0 +1,11 @@
+package com.projects.music.exception;
+
+import java.math.BigInteger;
+
+@SuppressWarnings("serial")
+public class SongNotFoundException extends RuntimeException {
+
+	public SongNotFoundException(final BigInteger id) {
+		super(String.format("song with id '%s' not found", id));
+	}
+}
